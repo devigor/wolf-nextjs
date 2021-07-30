da09454cc5089391e9e4e9a900bd55edd3634ef8
Igor Oliveira <devigor.tere@gmail.com>
2021-07-30 12:07:58 -0300

    refactor(index): rewrite the boilerplate to be use with styled components

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,22 +1,12 @@
-import Image from 'next/image'
-import wolfLogo from '../../public/wolf.png'
+import Container from 'components/Container'
+import Logo from 'components/Logo'
 
 export default function Home() {
   return (
-    <main className="container max-w-full mx-auto">
-      <div className="max-w-full h-screen flex justify-center items-center flex-col lg:flex-row">
-        <Image
-          src={wolfLogo}
-          alt="A wolf image"
-          placeholder="blur"
-          width={200}
-          height={200}
-        />
-
-        <h1 className="mt-4 font-mono text-6xl tracking-widest text-center uppercase">
-          Wolf NextJs
-        </h1>
-      </div>
-    </main>
+    <>
+      <Container>
+        <Logo />
+      </Container>
+    </>
   )
 }
